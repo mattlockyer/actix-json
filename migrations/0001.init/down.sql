95c60604f44c4a451cdf081ec1e5597c3bbd3fae
Matt Lockyer <mattdlockyer@gmail.com>
2019-05-27 06:23:01 -0700

    added down.sql

---
+++ migrations/0001.init/down.sql
@@ -0,0 +1,7 @@
+/********************************
+See up.sql for detailed instructions on set up for:
+1) a new user
+2) a schema and permissions
+3) a basic test table
+********************************/
+drop database test;
